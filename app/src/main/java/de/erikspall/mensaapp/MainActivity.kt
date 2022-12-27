1cda1a2aeaeecf243bcbc334b02213f1822c010d
Erik Spall <29352918+erikspall@users.noreply.github.com>
2022-12-27 23:29:25 +0100

    begin migration to jetpack compose

--- app/src/main/java/de/erikspall/mensaapp/MainActivity.kt
+++ app/src/main/java/de/erikspall/mensaapp/MainActivity.kt
@@ -1,29 +1,31 @@
 package de.erikspall.mensaapp
 
-import android.animation.ObjectAnimator
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.core.animation.doOnEnd
-import androidx.core.animation.doOnStart
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import dagger.hilt.android.AndroidEntryPoint
-import de.erikspall.mensaapp.databinding.ActivityMainBinding
+import de.erikspall.mensaapp.ui.compose.MensaApp
+import de.erikspall.mensaapp.ui.theme.ComposeMensaTheme
 
-@AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+//@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var navController: NavController
+    // private lateinit var binding: ActivityMainBinding
+    // private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // This will lay out our app behind the system bars
         WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent {
+
+            MensaApp()
+
+        }
+
+        /*WindowCompat.setDecorFitsSystemWindows(window, false)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -32,35 +34,35 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        setupBottomNavMenu(navController)
+        setupBottomNavMenu(navController)*/
     }
 
-    private fun setupBottomNavMenu(navController: NavController) {
-        val bottomNav = findViewById<BottomNavigationView>(R.id.nav_view)
-        bottomNav?.setupWithNavController(navController)
+    /* private fun setupBottomNavMenu(navController: NavController) {
+         val bottomNav = findViewById<BottomNavigationView>(R.id.nav_view)
+         bottomNav?.setupWithNavController(navController)
 
-            navController.addOnDestinationChangedListener { _, destination, _ ->
-                if(destination.id == R.id.cafe_list_dest || destination.id == R.id.mensa_list_dest || destination.id == R.id.settings_dest) {
-                    //val height = bottomNav.translationY
-                    ObjectAnimator.ofFloat(bottomNav, "translationY", 0f).apply {
-                        duration = 300
-                        doOnStart {
-                            bottomNav.visibility = View.VISIBLE
-                        }
-                        start()
-                    }
-                } else {
-                    ObjectAnimator.ofFloat(bottomNav, "translationY", bottomNav.height.toFloat()-50f).apply {
-                        duration = 300
-                        doOnEnd {
-                            bottomNav.visibility = View.GONE
-                        }
-                        start()
-                    }
-                }
-            }
-    }
+             navController.addOnDestinationChangedListener { _, destination, _ ->
+                 if(destination.id == R.id.cafe_list_dest || destination.id == R.id.mensa_list_dest || destination.id == R.id.settings_dest) {
+                     //val height = bottomNav.translationY
+                     ObjectAnimator.ofFloat(bottomNav, "translationY", 0f).apply {
+                         duration = 300
+                         doOnStart {
+                             bottomNav.visibility = View.VISIBLE
+                         }
+                         start()
+                     }
+                 } else {
+                     ObjectAnimator.ofFloat(bottomNav, "translationY", bottomNav.height.toFloat()-50f).apply {
+                         duration = 300
+                         doOnEnd {
+                             bottomNav.visibility = View.GONE
+                         }
+                         start()
+                     }
+                 }
+             }
+     }*/
 
-        //setContentView(R.layout.activity_main)
+    //setContentView(R.layout.activity_main)
 
 }
--- app/src/main/java/de/erikspall/mensaapp/ui/settings/mealcomponents/event/AllergenicEvent.kt
+++
@@ -1,9 +0,0 @@
-package de.erikspall.mensaapp.ui.settings.mealcomponents.event
-
-import de.erikspall.mensaapp.domain.enums.AdditiveType
-
-sealed class AllergenicEvent {
-    object Init: AllergenicEvent()
-    data class OnWarningsChanged(val warningsActivated: Boolean): AllergenicEvent()
-    data class OnAdditiveChecked(val name: String, val type: AdditiveType, val checked: Boolean): AllergenicEvent()
-}
