f2f3dc0e23af16efdb20f784071313d68c6feac4
Erik Spall <29352918+erikspall@users.noreply.github.com>
2022-12-31 16:24:15 +0100

    added slide in/out animation to filters

--- app/src/main/java/de/erikspall/mensaapp/ui/screens/additivefilters/AdditiveFilterScreen.kt
+++ app/src/main/java/de/erikspall/mensaapp/ui/screens/additivefilters/AdditiveFilterScreen.kt
@@ -1,7 +1,11 @@
 package de.erikspall.mensaapp.ui.screens.additivefilters
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Blender
@@ -10,7 +14,9 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,6 +36,10 @@ fun AdditiveFilterScreen(
     mensaViewModel: MensaViewModel = hiltViewModel()
 ) {
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val filterVisibilityState = remember {
+        MutableTransitionState(mensaViewModel.warningsActivated)
+    }
+
     val ingredients by mensaViewModel.ingredients.observeAsState()
     val allergens by mensaViewModel.allergens.observeAsState()
 
@@ -62,39 +72,47 @@ fun AdditiveFilterScreen(
             Column {
                 HeroToggle(
                     modifier = Modifier
-                        .padding(innerPadding)
-                        .padding(16.dp),
+                        .padding(top = 16.dp + innerPadding.calculateTopPadding(), start = 16.dp, end = 16.dp),
                     title = stringResource(id = R.string.text_hero_toggle_warnings_additives),
                     checked = mensaViewModel.warningsActivated,
                     onChecked = {
                         //switchChecked.value = it
                         mensaViewModel.enableWarnings(it)
+                        filterVisibilityState.targetState = it
                     }
                 )
-                if (mensaViewModel.warningsActivated) {
-                    AdditiveFilterSection(
-                        modifier = Modifier
-                            .padding(horizontal = 24.dp),
-                        icon = Icons.Rounded.Blender,
-                        sectionTitle = stringResource(id = R.string.text_ingredient_section),
-                        items = ingredients ?: emptyList(), // Make it null safe?
-                        onAdditiveClicked = {
-                            mensaViewModel.saveLikeStatus(it, !it.isNotLiked)
-                        }
-                    )
-                    Divider(
-                        modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp)
-                    )
-                    AdditiveFilterSection(
-                        modifier = Modifier
-                            .padding(horizontal = 24.dp),
-                        icon = Icons.Rounded.LunchDining,
-                        sectionTitle = stringResource(id = R.string.text_allergen_section),
-                        items = allergens ?: emptyList(), // Make it null safe?
-                        onAdditiveClicked = {
-                            mensaViewModel.saveLikeStatus(it, !it.isNotLiked)
-                        }
-                    )
+                AnimatedVisibility(
+                    modifier = Modifier.clip(RoundedCornerShape(corner = CornerSize(28.dp))), // Looks better
+                    visibleState = filterVisibilityState
+                ) {
+                    Column(
+                        modifier = Modifier.padding(top = 16.dp)
+                    ) {
+                        AdditiveFilterSection(
+                            modifier = Modifier
+                                .padding(horizontal = 24.dp),
+                            icon = Icons.Rounded.Blender,
+                            sectionTitle = stringResource(id = R.string.text_ingredient_section),
+                            items = ingredients ?: emptyList(), // Make it null safe?
+                            onAdditiveClicked = {
+                                mensaViewModel.saveLikeStatus(it, !it.isNotLiked)
+                            }
+                        )
+                        Divider(
+                            modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp)
+                        )
+                        AdditiveFilterSection(
+                            modifier = Modifier
+                                .padding(horizontal = 24.dp),
+                            icon = Icons.Rounded.LunchDining,
+                            sectionTitle = stringResource(id = R.string.text_allergen_section),
+                            items = allergens ?: emptyList(), // Make it null safe?
+                            onAdditiveClicked = {
+                                mensaViewModel.saveLikeStatus(it, !it.isNotLiked)
+                            }
+                        )
+                    }
+
                 }
 
             }
