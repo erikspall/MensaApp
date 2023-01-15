7c7dab96ee68fd85cd63f2683c6174d7c0bb508c
Erik Spall <29352918+erikspall@users.noreply.github.com>
2023-01-15 22:58:23 +0100

    add lotties for menus and additives

--- app/src/main/java/de/erikspall/mensaapp/ui/screens/foodproviders/FoodProvidersScreen.kt
+++ app/src/main/java/de/erikspall/mensaapp/ui/screens/foodproviders/FoodProvidersScreen.kt
@@ -47,55 +47,59 @@ fun FoodProvidersScreen(
         },
         content = { innerPadding ->
 
-            if (mensaViewModel.foodProviderScreenState == UiState.LOADING) {
-                Column(
-                    modifier = Modifier.fillMaxHeight(),
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    LottieWithInfo(
-                        lottie = R.raw.man_serving_catering_food,
-                        description = stringResource(
-                            id = when (foodProviderCategory) {
-                                Category.CANTEEN -> R.string.text_lottie_fetching_canteens
-                                Category.CAFETERIA -> R.string.text_lottie_fetching_cafeterias
-                                else -> R.string.text_lottie_error_fetching_canteens
-                            }
+            when (mensaViewModel.foodProviderScreenState) {
+                UiState.LOADING -> {
+                    Column(
+                        modifier = Modifier.fillMaxHeight(),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        LottieWithInfo(
+                            lottie = R.raw.man_serving_catering_food,
+                            description = stringResource(
+                                id = when (foodProviderCategory) {
+                                    Category.CANTEEN -> R.string.text_lottie_fetching_canteens
+                                    Category.CAFETERIA -> R.string.text_lottie_fetching_cafeterias
+                                    else -> R.string.text_lottie_error
+                                }
+                            )
                         )
-                    )
-                }
-            } else if (mensaViewModel.foodProviderScreenState == UiState.NO_INFO) {
-                Column(
-                    modifier = Modifier.fillMaxHeight(),
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    LottieWithInfo(
-                        lottie = R.raw.no_info,
-                        iterations = 1,
-                        description = stringResource(
-                            id = when (foodProviderCategory) {
-                                Category.CANTEEN -> R.string.text_lottie_no_info_canteens
-                                Category.CAFETERIA -> R.string.text_lottie_no_info_cafeterias
-                                else -> R.string.text_lottie_no_info_canteens
-                            }
-                        )
-                    )
-                }
-            } else {
-                FoodProvidersList(
-                    modifier = modifier
-                        .padding(
-                            innerPadding
-                        )
-                        .padding(bottom = MaterialSizes.BOTTOM_NAV_HEIGHT.dp),
-                    list = when (foodProviderCategory) {
-                        Category.CANTEEN -> mensaViewModel.canteens
-                        Category.CAFETERIA -> mensaViewModel.cafeterias
-                        else -> mensaViewModel.foodProviders
-                    }.sortedByDescending { it.liked },
-                    onClickedFoodProvider = { clickedProvider ->
-                        onFoodProviderClick(clickedProvider.id ?: -1)
                     }
-                )
+                }
+                UiState.NO_INFO -> {
+                    Column(
+                        modifier = Modifier.fillMaxHeight(),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        LottieWithInfo(
+                            lottie = R.raw.no_info,
+                            iterations = 1,
+                            description = stringResource(
+                                id = when (foodProviderCategory) {
+                                    Category.CANTEEN -> R.string.text_lottie_no_info_canteens
+                                    Category.CAFETERIA -> R.string.text_lottie_no_info_cafeterias
+                                    else -> R.string.text_lottie_no_info_canteens
+                                }
+                            )
+                        )
+                    }
+                }
+                else -> {
+                    FoodProvidersList(
+                        modifier = modifier
+                            .padding(
+                                innerPadding
+                            )
+                            .padding(bottom = MaterialSizes.BOTTOM_NAV_HEIGHT.dp),
+                        list = when (foodProviderCategory) {
+                            Category.CANTEEN -> mensaViewModel.canteens
+                            Category.CAFETERIA -> mensaViewModel.cafeterias
+                            else -> mensaViewModel.foodProviders
+                        }.sortedByDescending { it.liked },
+                        onClickedFoodProvider = { clickedProvider ->
+                            onFoodProviderClick(clickedProvider.id ?: -1)
+                        }
+                    )
+                }
             }
         }
     )
